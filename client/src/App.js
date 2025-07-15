1f2b77e9fd656d1d2dfe6b2ad89acaa9247d0dcd
Jake Riekert <jacob.riekert+github@gmail.com>
2025-07-15 12:34:18 -0400

    new commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+//import Navbar from './Navbar.js';
+import Home from './Home';
+import './index.css';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <Navbar /> */}
+            <Home />
+        </div>
+    );
+}
+
+export default App;
